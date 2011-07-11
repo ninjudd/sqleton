36b38f217a9dfb4ada8f0ebd341b24471fc40926
Justin Balthrop <git@justinbalthrop.com>
2011-07-11 14:37:13 -0700

    support system property overrides

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqleton "0.0.3"
+(defproject sqleton "0.0.5"
   :description "SQL tools for Clojure."
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]])
--- src/sqleton/connection.clj
+++ src/sqleton/connection.clj
@@ -6,32 +6,38 @@
 
 (def *datasource* nil)
 
-(defn load-user-config [name]
+(defn load-user-config [source-name]
   (let [overrides (java.io.File. (str (System/getProperty "user.home") "/.sqleton/" (:artifact-id *project*) ".clj"))]
     (when (.exists overrides)
-      (get (read-string (slurp overrides)) name))))
+      (get (read-string (slurp overrides)) source-name))))
+
+(defn system-overrides [source-name]
+  (into {} (for [key [:host :port :database :user :password]
+                 :let [value (System/getProperty (join "." (map name ["sqleton" source-name key])))]
+                 :when value]
+             [key value])))
 
 (defn datasource
   "Returns a map appropriate for either a jndi or traditional data source."
-  [name]
+  [source-name]
   (with-meta
-    (if (or (in-cake-jvm?) (nil? (get-in *project* [:db name :name])))
-      (let [defaults (get-in *project* [:db name])
-            config   (merge defaults (load-user-config name))
-            {:keys [host port database user password subprotocol]} config]
+    (if (or (in-cake-jvm?) (nil? (get-in *project* [:db source-name :name])))
+      (let [defaults (get-in *project* [:db source-name])
+            config   (merge defaults (load-user-config source-name) (system-overrides source-name))
+            {:keys [host port database user password]} config]
         {:classname   "org.postgresql.Driver"
-         :subprotocol subprotocol
-         :user        (or user (System/getProperty "user.name"))
+         :subprotocol "postgresql"
+         :user        (or (System/getProperty "sqleton") user (System/getProperty "user.name"))
          :password    password
          :subname     (str "//" host ":" (or port 5432) "/" database)})
-      (get-in *project* [:db name]))
-    {:name name}))
+      (get-in *project* [:db source-name]))
+    {:name source-name}))
 
 (defmacro with-db
   "Create a connection to a specific db if there is not one for this thread already."
-  [name & forms]
-  `(if (= ~name (:name (meta *datasource*)))
+  [source-name & forms]
+  `(if (= ~source-name (:name (meta *datasource*)))
      (do ~@forms)
-     (binding [*datasource* (datasource ~name)]
+     (binding [*datasource* (datasource ~source-name)]
        (with-connection *datasource*
          ~@forms))))
