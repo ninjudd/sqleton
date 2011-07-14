110624efdfd257e601e200cdab16bc7fa9054dbc
lance bradley <l@ncebradley.org>
2011-07-13 18:19:08 -0700

    better error reporting

--- src/sqleton/connection.clj
+++ src/sqleton/connection.clj
@@ -21,16 +21,16 @@
   "Returns a map appropriate for either a jndi or traditional data source."
   [source-name]
   (with-meta
-    (if (or (in-cake-jvm?) (nil? (get-in *project* [:db source-name :name])))
-      (let [defaults (get-in *project* [:db source-name])
-            config   (merge defaults (load-user-config source-name) (system-overrides source-name))
-            {:keys [host port database user password]} config]
-        {:classname   "org.postgresql.Driver"
-         :subprotocol "postgresql"
-         :user        (or (System/getProperty "sqleton") user (System/getProperty "user.name"))
-         :password    password
-         :subname     (str "//" host ":" (or port 5432) "/" database)})
-      (get-in *project* [:db source-name]))
+    (into {:classname   "org.postgresql.Driver"
+           :subprotocol "postgresql"}
+          (if (or (in-cake-jvm?) (nil? (get-in *project* [:db source-name :name])))
+            (let [defaults (get-in *project* [:db source-name])
+                  config   (merge defaults (load-user-config source-name) (system-overrides source-name))
+                  {:keys [host port database user password]} config]
+              {:user        (or (System/getProperty "sqleton") user (System/getProperty "user.name"))
+               :password    password
+               :subname     (str "//" host ":" (or port 5432) "/" database)})
+            (get-in *project* [:db source-name])))
     {:name source-name}))
 
 (defmacro with-db
@@ -39,5 +39,9 @@
   `(if (= ~source-name (:name (meta *datasource*)))
      (do ~@forms)
      (binding [*datasource* (datasource ~source-name)]
-       (with-connection *datasource*
-         ~@forms))))
+       (try
+         (with-connection *datasource*
+           ~@forms)
+         (catch java.sql.SQLException e#
+           (throw (java.sql.SQLException.
+                   (str "Could not connect to: " (pr-str *datasource*)))))))))
