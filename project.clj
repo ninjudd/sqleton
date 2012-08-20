94de196a0a351a2dc4f0b41e41218fbf1bf9b284
Justin Balthrop <git@justinbalthrop.com>
2012-08-20 11:58:43 -0700

    update to not depend on cake

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqleton "0.0.11"
+(defproject sqleton "0.1.0-alpha1"
   :description "SQL tools for Clojure."
-  :dependencies [[clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]])
--- src/sqleton/connection.clj
+++ src/sqleton/connection.clj
@@ -1,40 +1,38 @@
 (ns sqleton.connection
-  (:use cake
-        [bake.core :only [in-cake-jvm?]]
-        [clojure.string :only [join]]
+  (:use [clojure.string :only [join]]
         [clojure.java.jdbc :only [with-connection]]
         [useful.exception :only [exception-map]]))
 
 (def ^:dynamic *datasource* nil)
 
+(def config (atom {}))
+
 (def last-exception (atom false))
 
-(defn load-user-config [source-name]
-  (let [overrides (java.io.File. (str (System/getProperty "user.home") "/.sqleton/" (:artifact-id *project*) ".clj"))]
-    (when (.exists overrides)
-      (get (read-string (slurp overrides)) source-name))))
-
-(defn system-overrides [source-name]
+(defn overrides [source-name]
   (into {} (for [key [:host :port :database :user :password]
                  :let [value (System/getProperty (join "." (map name ["sqleton" source-name key])))]
                  :when value]
              [key value])))
 
+(defn add-subname [{:keys [host port database] :as datasource}]
+  (if (:subname datasource)
+    datasource
+    (assoc datasource
+      :subname (str "//" host ":" (or port 5432) "/" database))))
+
 (defn datasource
   "Returns a map appropriate for either a jndi or traditional data source."
   [source-name]
-  (with-meta
-    (into {:classname   "org.postgresql.Driver"
-           :subprotocol "postgresql"}
-          (if (or (in-cake-jvm?) (nil? (get-in *project* [:db source-name :name])))
-            (let [defaults (get-in *project* [:db source-name])
-                  config   (merge defaults (load-user-config source-name) (system-overrides source-name))
-                  {:keys [host port database user password]} config]
-              {:user        (or (System/getProperty "sqleton") user (System/getProperty "user.name"))
-               :password    password
-               :subname     (str "//" host ":" (or port 5432) "/" database)})
-            (get-in *project* [:db source-name])))
-    {:name source-name}))
+  (let [defaults (get-in @config [:db source-name])]
+    (with-meta
+      (into {:classname   "org.postgresql.Driver"
+             :subprotocol "postgresql"}
+            (if (:name defaults)
+              defaults
+              (add-subname (merge defaults
+                                  (overrides source-name)))))
+      {:name source-name})))
 
 (def pg-connection-error #{"08000" "08001" "08003" "08004" "08006" "28000" "3D000"})
 
