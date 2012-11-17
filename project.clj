111ebf8df7d2d0cc69759b77a9c404bb9ebc8e00
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 17:20:41 -0800

    add license and url

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject org.flatland/sqleton "0.2.0"
   :description "SQL tools for Clojure."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/flatland/sqleton"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.flatland/useful "0.9.0"]])
