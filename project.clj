f914de5e0945fbe22b2cece7a783afb94e6bc8a0
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 17:17:10 -0800

    0.2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.flatland/sqleton "0.1.1-SNAPSHOT"
+(defproject org.flatland/sqleton "0.2.0"
   :description "SQL tools for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]])
+                 [org.flatland/useful "0.9.0"]])
