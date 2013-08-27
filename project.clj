3253a0573f0c9a6f714d94ccafd08ca73d523cd1
Alan Malloy <alan@malloys.org>
2013-08-26 18:35:02 -0700

    0.2.1, with support for connection pooling via BoneCP

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject org.flatland/sqleton "0.2.0"
+(defproject org.flatland/sqleton "0.2.1"
   :description "SQL tools for Clojure."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/flatland/sqleton"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]
-                 [org.flatland/useful "0.9.0"]])
+                 [org.flatland/useful "0.9.0"]
+                 [com.jolbox/bonecp "0.7.1.RELEASE"]])
--- src/flatland/sqleton/connection.clj
+++ src/flatland/sqleton/connection.clj
@@ -1,11 +1,35 @@
 (ns flatland.sqleton.connection
   (:use [clojure.string :only [join]]
         [clojure.java.jdbc :only [with-connection]]
-        [flatland.useful.exception :only [exception-map]]))
+        [flatland.useful.exception :only [exception-map]])
+  (:import com.jolbox.bonecp.BoneCPDataSource))
 
 (def ^:dynamic *datasource* nil)
 
-(def config (atom {}))
+(defonce config (atom {}))
+
+(defn jdbc-path [{:keys [host port database]}]
+  (str "//" host ":" (or port 5432) "/" database))
+
+(defn add-subname [datasource]
+  (if (:subname datasource)
+    datasource
+    (assoc datasource
+      :subname (jdbc-path datasource))))
+
+(defn jdbc-uri [config]
+  (str "jdbc:postgresql:" (jdbc-path config)))
+
+(defn bonecp-datasource* [{:keys [host port database user password] :as config}]
+  (let [ds (BoneCPDataSource.)]
+    (.setJdbcUrl ds (jdbc-uri config))
+    (when user (.setUsername ds user))
+    (when password (.setPassword ds password))
+    ds))
+
+(def bonecp-datasource (memoize (fn [config]
+                                  (assoc config
+                                    :datasource (bonecp-datasource* config)))))
 
 (def last-exception (atom false))
 
@@ -15,24 +39,19 @@
                  :when value]
              [key value])))
 
-(defn add-subname [{:keys [host port database] :as datasource}]
-  (if (:subname datasource)
-    datasource
-    (assoc datasource
-      :subname (str "//" host ":" (or port 5432) "/" database))))
-
 (defn datasource
   "Returns a map appropriate for either a jndi or traditional data source."
   [source-name]
   (let [defaults (get-in @config [:db source-name])]
-    (with-meta
-      (into {:classname   "org.postgresql.Driver"
-             :subprotocol "postgresql"}
-            (if (:name defaults)
-              defaults
-              (add-subname (merge defaults
-                                  (overrides source-name)))))
-      {:name source-name})))
+    (-> (if-let [pool-config (:pooled defaults)]
+          (bonecp-datasource (merge pool-config (overrides source-name)))
+          (into {:classname   "org.postgresql.Driver"
+                 :subprotocol "postgresql"}
+                (if (:name defaults)
+                  defaults
+                  (add-subname (merge defaults
+                                      (overrides source-name))))))
+        (with-meta {:name source-name}))))
 
 (def pg-connection-error #{"08000" "08001" "08003" "08004" "08006" "28000" "3D000"})
 
